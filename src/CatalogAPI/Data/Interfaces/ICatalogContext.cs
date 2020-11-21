d0e1a0b613085cecc4f48f92db66e511cdf07087
Jargalbat <jargalbat@gmail.com>
2020-11-21 15:29:44 +0800

    first commit

---
+++ src/CatalogAPI/Data/Interfaces/ICatalogContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using CatalogAPI.Entities;
+using MongoDB.Driver;
+
+namespace CatalogAPI.Data.Interfaces
+{
+    public interface ICatalogContext
+    {
+        IMongoCollection<Product> Products { get; }
+    }
+}
